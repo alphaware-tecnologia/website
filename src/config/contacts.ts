4a955309e8e25f3abdad360927fa006cc31d0692
Jonathan <jonathan15989@protonmail.com>
2026-01-07 20:18:17 -0300

    fix: update email domain in contacts configuration

--- src/config/contacts.ts
+++ src/config/contacts.ts
@@ -1,6 +1,6 @@
 const contacts = {
   phone: "+55 (31) 98489-4667",
-  email: "alphaware_tecnologia@protonmail.com",
+  email: "alphaware_tecnologia@proton.me",
   whatsappLink: "https://wa.me/message/W3DVZAVZXYEWL1",
 };
 
